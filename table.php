fafcf5dbd90eba903250e0aefd8738db25e7e354
Rasmus Kivipõld <sizen@greeny.cs.tlu.ee>
2015-10-16 13:28:01 +0300

    nüüd saab editida tabeli aknas

--- table.php
+++ table.php
@@ -2,6 +2,12 @@
 
 
 	require_once("functions.php");
+	require_once("edit_functions.php");
+	
+	//kasutaja tahab midagi muuta
+	if(isset($_POST["update"])){
+		updateCar($_POST["id"], $_POST["number_plate"], $_POST["color"]);
+	}
 	
 	//kas kasutaja tahab kustutada
 	//kas aadressireal on ?delete=?!?!!!?!
@@ -38,9 +44,10 @@
 			if(isset($_GET["edit"]) && $car_list[$i]->id == $_GET["edit"]){
 				//kasutajale muutmiseks
 				echo "<tr>";
-					echo "<form>";
+					echo "<form action=table.php method='post'>";
 						echo "<td>".$car_list[$i]->id."</td>";
 						echo "<td>".$car_list[$i]->user_id."</td>";
+						echo "<td><input type='hidden' name='id' value='".$car_list[$i]->id."'></td>";
 						echo "<td><input name='number_plate' value='".$car_list[$i]->number_plate."'></td>";
 						echo "<td><input name='color' value='".$car_list[$i]->color."'></td>";
 						echo "<td><input type='submit' name='update'></td>";
@@ -70,6 +77,6 @@
 	
 	?>
 	
-
-
+	
+	
 </table>
